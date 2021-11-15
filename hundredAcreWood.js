1926898dc023ed3d69cd26722be2fc080a94121d
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-11-15 10:47:21 -0600

    first commit

---
+++ hundredAcreWood.js
@@ -0,0 +1,46 @@
+let tigger = {character: 'Tigger'}
+let pooh = {character:'Winnie the Pooh'}
+let piglet = {character:'Piglet'}
+let owl = {character:'Owl'}
+let cRobin = {character:'Christopher Robin'}
+let rabbit = {character: 'Rabbit'}
+let gopher = {character: 'Gopher'}
+let bees = {character:'Bees'}
+let konga ={character: 'Konga'}
+let eeyore = {character:'Eeyore'}
+let heffalumps ={character:'Heffalumps'}
+
+tigger['north'] = pooh
+pooh['south'] = tigger
+piglet['east']=pooh
+pooh['west']=piglet
+owl['south']=piglet
+piglet['north']=owl
+owl['east']=cRobin
+cRobin['north']=konga
+konga['south']=cRobin
+cRobin['east']=rabbit
+rabbit['west']=cRobin
+cRobin['south']=pooh
+pooh['north']=cRobin
+bees['west']=pooh
+pooh['east']=bees
+bees['north']=rabbit
+rabbit['south']=bees
+rabbit['east']=gopher
+gopher['west']=rabbit
+cRobin['north']=konga
+konga['south']=cRobin
+konga['north']=eeyore
+eeyore['south']=konga
+eeyore['east']=heffalumps
+heffalumps['west']=eeyore
+
+let player={location:tigger}
+
+function move(direction){
+
+    player.location = player.location[direction]
+    console.log(`You are now at ${player.location.character}'s house`)
+
+}
